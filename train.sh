0cc332a318ad5d0842a766e9f22644732badfd31
kscibi <scibiliakevin@gmail.com>
2020-09-22 13:11:35 +0200

    further additions (testing)

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 main.py --is_3d=True --dataset 'tumor_mparam' --res_x=64 --res_y=64 --res_z=64 --num-conv 4 --batch_size=16 --num_worker=8 --log_ep=5 --test_ep=5 --use_curl False  --arch 'alternative' --phys_loss False --is_train True --gpu_id="2" --max_epoch 30 --tag 'clipped_0.001_geom_3_extracsf' --valid_dataset_dir '/mnt/Drive2/ivan/data/valid' --log_dir '/mnt/Drive2/ivan_kevin/log' --data_dir '/mnt/Drive2/ivan/data'
+python3 main.py --is_3d=True --dataset 'tumor_mparam' --res_x=128 --res_y=128 --res_z=128 --num-conv 4 --batch_size=16 --num_worker=8 --log_ep=5 --test_ep=5 --use_curl False  --arch 'alternative' --phys_loss False --is_train True --gpu_id="4" --max_epoch 30 --tag 'inverse_test' --valid_dataset_dir '/mnt/Drive2/ivan/data/valid' --log_dir '/mnt/Drive2/ivan_kevin/log' --data_dir '/mnt/Drive2/ivan/data'
 
