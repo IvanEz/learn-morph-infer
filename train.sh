94e9b08a231d8861e4e0319a8578d7ede7e28eea
kscibi <scibiliakevin@gmail.com>
2020-09-22 13:17:02 +0200

    testing commit (not crucial)

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 main.py --is_3d=True --dataset 'tumor_mparam' --res_x=128 --res_y=128 --res_z=128 --num-conv 4 --batch_size=16 --num_worker=8 --log_ep=5 --test_ep=5 --use_curl False  --arch 'alternative' --phys_loss False --is_train True --gpu_id="4" --max_epoch 30 --tag 'inverse_test' --valid_dataset_dir '/mnt/Drive2/ivan/data/valid' --log_dir '/mnt/Drive2/ivan_kevin/log' --data_dir '/mnt/Drive2/ivan/data'
+python3 main.py --is_3d=True --dataset 'tumor_mparam' --res_x=128 --res_y=128 --res_z=128 --filters=32 --num-conv 4 --batch_size=16 --num_worker=8 --log_ep=5 --test_ep=5 --use_curl False  --arch 'alternative' --phys_loss False --is_train True --gpu_id="4" --max_epoch 30 --tag 'inverse_test' --valid_dataset_dir '/mnt/Drive2/ivan/data/valid' --log_dir '/mnt/Drive2/ivan_kevin/log' --data_dir '/mnt/Drive2/ivan/data'
 
