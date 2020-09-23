0978553d72825e2b5ba543f2b0b25616db7cffe5
kscibi <scibiliakevin@gmail.com>
2020-09-23 18:40:01 +0200

    debugging

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 main.py --is_3d=True --dataset 'samples_extended' --res_x=128 --res_y=128 --res_z=128 --filters=32 --num-conv 4 --batch_size=8 --num_worker=8 --log_ep=5 --test_ep=5 --use_curl False  --arch 'alternative' --phys_loss False --is_train True --gpu_id="0" --max_epoch 30 --tag 'inverse_test_dbggd' --valid_dataset_dir '/mnt/Drive2/ivan/data/valid' --log_dir '/mnt/Drive2/ivan_kevin/log' --data_dir '/mnt/Drive2/ivan_kevin/testingstuff'
+python3 main.py --is_3d=True --dataset 'samples_extended' --res_x=128 --res_y=128 --res_z=128 --filters=32 --num-conv 4 --batch_size=12 --num_worker=8 --log_ep=5 --test_ep=5 --use_curl False  --arch 'alternative' --phys_loss False --is_train True --gpu_id="0" --max_epoch 30 --tag 'inverse_test_dbggd' --valid_dataset_dir '/mnt/Drive2/ivan/data/valid' --log_dir '/mnt/Drive2/ivan_kevin/log' --data_dir '/mnt/Drive2/ivan_kevin/testingstuff'
 
