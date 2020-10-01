e6381e2f0a4106bf7452c6cd364e85f8aca80c6f
kescibi <scibilia@in.tum.de>
2020-10-01 18:03:07 +0000

    train.sh has fast 'benchmark'

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 main.py --random_seed=84905 --outputparams=6 --filters=32 --num_conv=3 --log_step=30 --batch_size=12 --num_worker=8 --is_train True --gpu_id="1" --max_epoch=20 --lr_max=0.0003 --beta1=0.9 --tag 'inverse_test_notthresholded_sanity' --log_dir '/mnt/Drive2/ivan_kevin/log' --data_dir '/mnt/Drive2/ivan_kevin/samples_extended_copy' --dataset 'Dataset'
+python3 main.py --random_seed=12345 --endtraindata=12 --startvaldata=12 --endvaldata=16 --outputparams=6 --filters=32 --num_conv=3 --log_step=1 --batch_size=4 --num_worker=8 --is_train True --gpu_id="1" --max_epoch=80 --lr_max=0.0003 --lr_min=0.00003 --beta1=0.9 --tag 'inverse_test_notthresholded_sanity_cleanertest' --log_dir '/mnt/Drive2/ivan_kevin/log' --data_dir '/mnt/Drive2/ivan_kevin/samples_extended_copy' --dataset 'Dataset'
 
