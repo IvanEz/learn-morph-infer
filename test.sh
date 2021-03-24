2bfc170695b744f5df6ef8e5fbc040f1e6d4e2d6
kescibi <70854775+kescibi@users.noreply.github.com>
2021-03-24 19:46:43 +0100

    Delete test.sh

--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3 main.py --is_train=False --filters=32 --load_path=/mnt/Drive2/ivan_kevin/log/samples_extended/0926_105118_alternative_inverse_test_lr0.009-0.0007_stdbt_4x4x4_numconv3_l2 --test_batch_size=1 --is_3d=True --dataset='TestDataset' --res_x=128 --res_y=128 --res_z=128 --batch_size=16 --num_worker=1 --num-conv 3 --use_curl False --arch 'alternative' --phys_loss False --gpu_id="4" --data_dir=/mnt/Drive2/ivan_kevin/testsamples --inf_save=/mnt/Drive2/ivan_kevin/testsamples_results
